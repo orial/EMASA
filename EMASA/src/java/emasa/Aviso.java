a3402158954b420888dc8a3d92bec0bd8db34916
Lupicinio García Ortiz <orialuga@gmail.com>
2016-04-11 13:15:16 +0200

    Nuevo proyecto

---
+++ EMASA/src/java/emasa/Aviso.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emasa;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Lupi
+ */
+@Entity
+public class Aviso implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idAviso;
+    
+    
+
+    public Long getIdAviso() {
+        return idAviso;
+    }
+
+    public void setIdAviso(Long idAviso) {
+        this.idAviso = idAviso;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idAviso != null ? idAviso.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the idAviso fields are not set
+        if (!(object instanceof Aviso)) {
+            return false;
+        }
+        Aviso other = (Aviso) object;
+        if ((this.idAviso == null && other.idAviso != null) || (this.idAviso != null && !this.idAviso.equals(other.idAviso))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "emasa.Aviso[ id=" + idAviso + " ]";
+    }
+    
+}
