62ddfc6c1184508e87758c15229fa444a2453d95
ManuelAlej <malex18jerez@gmail.com>
2016-05-05 09:47:12 +0200

    He cambiado @sessionscoped por requestscope

--- ProyectoEmasaTarea2/ProyectoEmasaTarea2/src/java/emasa/vistas/ControlAvisos.java
+++ ProyectoEmasaTarea2/ProyectoEmasaTarea2/src/java/emasa/vistas/ControlAvisos.java
@@ -14,6 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -29,7 +30,7 @@ import org.primefaces.event.UnselectEvent;
  */
 
 @Named(value="controlAvisos")
-@SessionScoped
+@RequestScoped
 public class ControlAvisos implements Serializable{
 
     @Inject
