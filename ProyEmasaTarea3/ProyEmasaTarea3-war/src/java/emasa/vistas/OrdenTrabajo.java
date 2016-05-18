5808216b3df5c090349a29b6657217b0e90e016d
ManuelAlej <malex18jerez@gmail.com>
2016-05-18 19:25:25 +0200

    Arreglado EJB Manuel OrdTrabajo
    Arreglado EJB Manuel OrdTrabajo

--- ProyEmasaTarea3/ProyEmasaTarea3-war/src/java/emasa/vistas/OrdenTrabajo.java
+++ ProyEmasaTarea3/ProyEmasaTarea3-war/src/java/emasa/vistas/OrdenTrabajo.java
@@ -6,6 +6,7 @@
 package emasa.vistas;
 
 import emasa.entidades.OrdTrabajo;
+import emasa.negocio.OrdTrabajoNegocio;
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
@@ -24,7 +26,7 @@ import javax.inject.Inject;
  */
 @Named(value = "ordenTrabajo")
 @SessionScoped
-@EJB OrdTrabajoNegocio ordTrabajoNegocio;
+
 
 public class OrdenTrabajo implements Serializable {
 
@@ -32,6 +34,7 @@ public class OrdenTrabajo implements Serializable {
     private CalendarioVista calendario;
     @Inject
     private OpcionesAviso aviso;
+    @EJB OrdTrabajoNegocio ordTrabajoNegocio;
 
     public CalendarioVista getCalendario() {
         return calendario;
